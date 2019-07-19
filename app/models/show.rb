5191e169a8155b119e22f266bf92a3b4f2f9e482
Trish Ogen <patriciaogen@gmail.com>
2019-07-19 02:57:04 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -7,4 +7,8 @@ class Show < ActiveRecord::Base
   def self.most_popular_show
     self.where("rating = ?", self.highest_rating).first
   end
+
+  def self.lowest_rating
+    self.minimum(:rating)
+  end
 end
